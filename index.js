09554f2bac7265502622c7785176524592be6b31
Victor Ng <victor@crankycoder.com>
2016-05-24 13:55:01 -0400

    grabbed the time for last access of a page.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const {Cc, Ci, Cu, Cr, Cm, components} = require("chrome");
 var self = require("sdk/self");
+var { setTimeout } = require("sdk/timers");
+
 
 // a dummy function, to show how tests work.
 // to see how to test this function, look at test/test-index.js
@@ -17,22 +19,28 @@ function dummy() {
     // execute the query
     var historyResult = historyService.executeQuery(query, options);
 
-    var historyRoot = historyResult.root;
-    historyRoot.containerOpen = true;
+    var root = historyResult.root;
+    root.containerOpen = true;
 
     // Iterate over the results
-    for (let i = 0; i < historyRoot.childCount; i++) {
+    for (let i = 0; i < root.childCount; i++) {
         let node = root.getChild(i);
         let uri = node.uri;
         let title = node.title;
         let icon = node.icon;
-        console.log("["+title+"] - ("+uri+")");
+        let lastAccessTime = node.time;
+        let node_type = node.type; // I think we only want type 2 RESULT_TYPE_FULL_VISIT
+        console.log("("+node_type+")" + lastAccessTime+": ["+uri+"] - ("+title+")");
     }
-    historyRoot.containerOpen = false;
+    root.containerOpen = false;
+
+    setTimeout(function() {
+        dummy();
+    }, 1000);
+    console.log("Scheduled a dummy() run");
 }
 
 dummy();
-
 
 console.log("============== dummy worked!");
 exports.dummy = dummy;
