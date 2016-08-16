8089ff05466e04a7df5763a2ab9e33d027067c62
Victor Ng <victor@crankycoder.com>
2016-08-16 17:02:12 -0400

    dropped event-constants removed noisey console.log messages

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-console.log("addon is starting");
-
 const xutils = require("shield-studies-addon-utils");
 const variationsMod = require("./variations");
 
@@ -21,23 +19,27 @@ var libdomwatch = require("./lib/domwatch");
 var libTabTracker = require("./lib/TabTracker");
 var simple_prefs = require("sdk/simple-prefs");
 
-console.log("*************************** heatmap started");
-
 /* 2. configuration / setup constants for the study.
  *  These are only ones needed, or supported
  */
-// TODO compute days until experiment end
+
+function daydiff(first, second) {
+    return Math.round((second-first)/(1000*60*60*24));
+}
+
+const STUDY_END_DATE = new Date(2017, 1, 15);
+
 const forSetup = {
   name: require("sdk/self").id, // unique for Telemetry
   choices: Object.keys(variationsMod.variations), // names of branches.
-  duration: 90,   // in days,
+  duration: daydiff(STUDY_END_DATE, new Date()),   // in days,
   /* Get surveyUrl from Strategy + Insights */
   surveyUrl: "https://qsurvey.mozilla.com/s3/Shield-Study-Example-Survey"
 };
 
 // 3. Study Object (module singleton);
 var ourConfig = xutils.xsetup(forSetup);
-let thisStudy = new xutils.Study(ourConfig,variationsMod);
+let thisStudy = new xutils.Study(ourConfig, variationsMod);
 
 // 3a (optional). Watch for changes and reporting
 xutils.Reporter.on("report",(d)=>console.debug("telemetry", d));
@@ -46,10 +48,7 @@ thisStudy.on("change",(newState)=>console.debug("newState:", newState));
 
 // Register an update listener
 function onPrefChange(prefName) {
-    console.log("The preference " + prefName + " value has changed!");
     let newPrefVal = simple_prefs.prefs[prefName];
-    console.log("New pref value: [" + newPrefVal + "]");
-    // TODO: change this to user Services.notifyObservers
     libheatmap.updatePrefs(newPrefVal);
 }
 
@@ -61,7 +60,6 @@ function main(options, callback) {
 
   // addon specific load code should go here, if there is additional.
   // We only want a single tab tracker instance globally
-  console.log("tab tracker is starting up");
   var tabTracker = new libTabTracker.TabTracker();
   libheatmap.main();
   simple_prefs.on("black_list", onPrefChange);
@@ -78,8 +76,6 @@ simple_prefs.on("deleteData", function() {
 
 function onUnload(reason) {
     // Valid reasons are: install, enable, startup, upgrade, downgrade
-    //
-    console.log("heatmap is unloading because: ["+reason+"]");
     xutils.handleOnUnload(reason, thisStudy);
     var unload_reasons = ['disable', 'uninstall'];
 
